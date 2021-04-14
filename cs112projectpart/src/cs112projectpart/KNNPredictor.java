27f3f9ab66dd546bd81e1e42a5fb745f253e4814
kyle <kpinuela@dons.usfca.edu>
2021-04-14 00:27:15 -0700

    test commit

---
+++ cs112projectpart/src/cs112projectpart/KNNPredictor.java
@@ -0,0 +1,31 @@
+package cs112projectpart;
+
+import java.util.ArrayList;
+
+public class KNNPredictor extends Predictor {
+
+	@Override
+	ArrayList<DataPoint> readData(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	String test(DataPoint data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Double getAccuracy(ArrayList<DataPoint> data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Double getPrecision(ArrayList<DataPoint> data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
